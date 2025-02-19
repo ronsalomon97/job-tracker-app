527aaa1a5b3deb0361aea8b714d7196d65a62680
Ron Salomon <ronsalomon97@gmail.com>
2025-02-19 18:14:28 +0200

    fixed some issues with the TransitionChild

--- frontend/src/components/JobFormModal.jsx
+++ frontend/src/components/JobFormModal.jsx
@@ -1,39 +1,50 @@
 // src/components/JobFormModal.jsx
-import React from 'react';
-import { Dialog, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
-import JobForm from './JobForm';
+import React, { Fragment } from 'react';
+import { Dialog, Transition, TransitionChild } from '@headlessui/react';
+import JobForm from './JobForm.jsx';
 
 function JobFormModal({ mode, initialData, onSubmit, onClose }) {
   return (
     <Transition appear show as={Fragment}>
-      <Dialog as='div' className="modal-overlay" onClose={onClose}>
-        <div className='modal-container'>
-          <Transition.Child
+      <Dialog as="div" className="modal-overlay" onClose={onClose}>
+        <div className="modal-container">
+          <TransitionChild
             as={Fragment}
-            enter="modal-overlay-enter" enterFrom="modal-overlay-from" enterTo="modal-overlay-to"
-            leave="modal-overlay-leave" leaveFrom="modal-overlay-leave-from" leaveTo="modal-overlay-leave-to"
+            enter="modal-overlay-enter"
+            enterFrom="modal-overlay-from"
+            enterTo="modal-overlay-to"
+            leave="modal-overlay-leave"
+            leaveFrom="modal-overlay-leave-from"
+            leaveTo="modal-overlay-leave-to"
           >
             <Dialog.Overlay className="modal-backdrop" />
-          </Transition.Child>
+          </TransitionChild>
 
-          {/* This invisible span is a trick used to help vertically center the modal content in the viewport. */}
+          {/* This invisible span helps vertically center the modal content */}
           <span className="modal-center-helper" aria-hidden="true">&#8203;</span>
 
-          <Transition.Child
+          <TransitionChild
             as={Fragment}
-            enter="modal-content-enter" enterFrom="modal-content-from" enterTo="modal-content-to"
-            leave="modal-content-leave" leaveFrom="modal-content-leave-from" leaveTo="modal-content-leave-to"
+            enter="modal-content-enter"
+            enterFrom="modal-content-from"
+            enterTo="modal-content-to"
+            leave="modal-content-leave"
+            leaveFrom="modal-content-leave-from"
+            leaveTo="modal-content-leave-to"
           >
             <div className="modal-content">
               <Dialog.Title className="modal-title">
                 {mode === 'add' ? 'Add New Job' : 'Edit Job'}
               </Dialog.Title>
               <div className="modal-body">
-                <JobForm initialData={initialData} onSubmit={onSubmit} onCancel={onClose} />
+                <JobForm 
+                  initialData={initialData}
+                  onSubmit={onSubmit}
+                  onCancel={onClose}
+                />
               </div>
             </div>
-          </Transition.Child>
+          </TransitionChild>
         </div>
       </Dialog>
     </Transition>
