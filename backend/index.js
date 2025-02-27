1a51e79ca57766a1a7afdf4449398b12d190ca2f
Ron Salomon <ronsalomon97@gmail.com>
2025-02-27 21:23:32 +0200

    Fixed Docker problems - finally

--- backend/index.js
+++ backend/index.js
@@ -24,4 +24,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log("Attempting to start server...");
+console.log("PORT:", PORT);
+app.listen(PORT, '0.0.0.0' , () => console.log(`Server running on port ${PORT}`));
