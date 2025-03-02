6eb800c6b6a6cd485c7da3a2f90b231854364ff1
Ron Salomon <ronsalomon97@gmail.com>
2025-03-02 23:48:21 +0200

    Changes ssh key for docker-compose - security reasons + modified axios.js - removing traling duplicates

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-});
+    baseURL: import.meta.env.VITE_API_URL?.replace(/\/$/, ""), // Removes trailing slash if exists
+  });
 
 console.log("VITE_API_URL Loaded:", import.meta.env.VITE_API_URL);
+
 API.interceptors.request.use(config => {
   const token = localStorage.getItem("token");
   if (token) {
